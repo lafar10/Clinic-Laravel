e634599677d2aec1045183664f581224a233e6be
Your Name <you@example.com>
2022-08-24 19:50:23 +0100

    use errorlog

--- config/logging.php
+++ config/logging.php
@@ -55,8 +55,8 @@ return [
         ],
 
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'driver' => 'errorlog',
+            
             'level' => env('LOG_LEVEL', 'debug'),
         ],
 
