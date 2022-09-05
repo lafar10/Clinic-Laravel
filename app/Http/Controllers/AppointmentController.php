6c1482400c456a8f8d9adc4e6351b22892cbd105
Lafar Ayoub <76670803+lafar10@users.noreply.github.com>
2022-09-05 20:29:34 +0100

    Update AppointmentController.php

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -58,7 +58,7 @@ class AppointmentController extends Controller
                 'tele' => $request->input('tele'),
                 'type_maladie' => $request->input('type_maladie'),
                 'montant' => $request->input('montant'),
-                'date_appointment' => $request->input('date_appointment')->format('d-m-y'),
+                'date_appointment' => date('d-m-Y', strtotime($request->input('date_appointment'))),
                 'heure_appointment' => $request->input('heure_appointment'),
                 'status' => $request->input('status'),
             ]);
