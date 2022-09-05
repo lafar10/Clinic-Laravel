adaf0a0e2b833a9d469f49551a3b4927c756e4a7
Lafar Ayoub <76670803+lafar10@users.noreply.github.com>
2022-09-05 20:02:10 +0100

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
 
 class UserSeeder extends Seeder
 {
