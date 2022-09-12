62591240641f88e56587e90e5b2f3d9dc5b6c8c8
Lafar Ayoub <76670803+lafar10@users.noreply.github.com>
2022-09-12 03:05:16 +0100

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
 <div class="container">
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -66,7 +73,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-info">
+                                <button type="submit" class="btn btn-outline-danger">
                                     {{ __('Register') }}
                                 </button>
                                 &nbsp;&nbsp;<a href="{{route('login')}}" class="btn btn-danger">
