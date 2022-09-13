41f106011d4eed8f3e04271655b81657d26ee2c7
Lafar Ayoub <76670803+lafar10@users.noreply.github.com>
2022-09-13 02:03:38 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,15 +52,55 @@
                     <a class="nav-link " href="#cont">Contact Us</a>
                   </li>
                 </ul>
-                <form class="d-flex" role="search">
+                <ul>
+                    @guest
+                        @if (Route::has('login'))
+                            <li>
+                                <a href="{{route('login')}}" class="btn btn-outline-danger">Login</a>
+                            </li>    
+                        @endif
+                        @if (Route::has('register'))
+                           <li>
+                                <a class="btn btn-danger" href="{{route('register')}}">Register</a>
+                           </li>
+                        @endif
+                    @else
 
-                        <a href="{{route('login')}}" class="btn btn-outline-danger">Login</a>
-                        <a class="btn btn-danger" href="{{route('register')}}">Register</a>
-                      </form>
+                        <li >
+                              <a id="navbarDropdown" class="dropdown-toggle" style="color:orange;" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                                                  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                                              </svg> {{ Auth::user()->name }}
+                              </a>
 
-              </div>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                             <a class="dropdown-item" href="{{ route('Dashboard') }}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-home" viewBox="0 0 16 16">
+                                                   <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                                                   <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                                                  </svg> Dashboard
+                            </a>
 
-            </div>
+				            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                                <svg xmlns="http://www.w3.org/2000/svg" style="color:orange;"  width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+                                <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                                </svg>   {{ __('home.logout') }}
+                                </a>
+
+                                   <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                             @csrf
+                                   </form>
+                                </div>
+                    </li>
+                @endguest
+                    </ul>
+
+                    </div>
+
+                </div>
           </nav>
                     <main  >
                         @yield('content')
