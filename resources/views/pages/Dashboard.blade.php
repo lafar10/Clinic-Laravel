fd4021c88d129f11479408f74cf90f3f84635f6f
Lafar Ayoub <76670803+lafar10@users.noreply.github.com>
2022-09-30 00:17:27 +0100

    Update Dashboard.blade.php

--- resources/views/pages/Dashboard.blade.php
+++ resources/views/pages/Dashboard.blade.php
@@ -6,7 +6,18 @@
 
         <div class="container " >
             <div class="row shadow-lg" id="fl">
-            
+                <div class="col-lg-4 bg-white" >
+                    <a href="{{route('appointment.status')}}"id="flx">
+                        <h4 class="display-5"><i style="color:rgb(240, 98, 98);" class="now-ui-icons ui-1_calendar-60"></i>&nbsp;&nbsp;Appointments</h4>
+                        <h2 class="display-5">{{ $appointments }}</h2>
+                    </a>
+                </div>
+                 <div class="col-lg-4 bg-white" >
+                    <a href="{{route('appointment.status')}}"id="flx">
+                        <h4 class="display-5"><i style="color:rgb(240, 98, 98);" class="now-ui-icons ui-1_calendar-60"></i>&nbsp;&nbsp;Appointments</h4>
+                        <h2 class="display-5">{{ $appointments1 }}</h2>
+                    </a>
+                </div>
                 <div class="col-lg-4 bg-white" >
                     <a href="{{route('payments')}}"id="flx">
                         <h4 class="display-5"><i style="color:rgb(240, 98, 98);" class="now-ui-icons business_money-coins"></i>&nbsp;Payments Total</h4>
