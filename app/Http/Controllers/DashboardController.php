4672d5c857176c9fffb0fb5033e1985ba075502e
Lafar Ayoub <76670803+lafar10@users.noreply.github.com>
2022-09-30 00:19:12 +0100

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,7 +13,7 @@ class DashboardController extends Controller
          $appointments = Appointment::where('date_appointment', Carbon::now()->format('d-m-y'))->where('status','En Attente')->get();
          $appointments1 = Payment::where('date_appointment', Carbon::today())->where('status','Payed')->sum('montant')->get();
        
-         return view('pages.Dashboard',compact('appointments','appointments1));
+         return view('pages.Dashboard',compact('appointments','appointments1');
     }
 
     public function indice()
