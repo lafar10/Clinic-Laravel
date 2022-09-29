99000ab122898f4e8f71959db3a06cd182763b3a
Lafar Ayoub <76670803+lafar10@users.noreply.github.com>
2022-09-30 00:03:07 +0100

    Update Dashboard.blade.php

--- resources/views/pages/Dashboard.blade.php
+++ resources/views/pages/Dashboard.blade.php
@@ -6,12 +6,7 @@
 
         <div class="container " >
             <div class="row shadow-lg" id="fl">
-               <!-- <div class="col-lg-4 bg-white" >
-                    <a href="{{route('appointment.status')}}"id="flx">
-                        <h4 class="display-5"><i style="color:rgb(240, 98, 98);" class="now-ui-icons ui-1_calendar-60"></i>&nbsp;&nbsp;Appointments</h4>
-                        <h2 class="display-5">{{\App\Models\Appointment::where('status','En Attente')->where('date_appointment',\Carbon\Carbon::now()->format('D-M-Y'))->count()}}</h2>
-                    </a>
-                </div> -->
+              
 
                 <div class="col-lg-4 bg-white" >
                     <a href="{{route('payments')}}"id="flx">
@@ -20,12 +15,6 @@
                     </a>
                 </div>
 
-                <div class="col-lg-4 bg-white" >
-                    <a href="{{route('payment.status')}}"id="flx">
-                        <h4 class="display-5"><i style="color:rgb(240, 98, 98);" class="now-ui-icons business_money-coins"></i>&nbsp; Today Total</h4>
-                        <h2 class="display-5">{{\App\Models\Payment::where('status','Payed')->where('created_at',\Carbon\Carbon::today())->sum('montant')}} <strong>DH's</strong></h2>
-                    </a>
-                </div>
             </div>
         </div>
         <br>
